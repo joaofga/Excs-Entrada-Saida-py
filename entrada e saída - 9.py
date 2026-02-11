c39040269972f3e484904cecf14df020d7baff64
João Franklin Goes Amorin <jaofga@gmail.com>
2026-02-11 15:04:24 -0300

    Create entrada e saída - 9.py

---
+++ entrada e saída - 9.py
@@ -0,0 +1,5 @@
+# 9. Faça um programa que calcule e mostre a área de um triângulo. Sabe-se que: Área = (base * altura) / 2.
+base = float(input('Digite a medida da base do triangulo: '))
+altura = float(input('Digite a altura do triangulo: '))
+area = (base * altura) / 2
+print('A área do triangulo é: ',area)
