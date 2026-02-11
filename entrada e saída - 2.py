8a189e38075646c7e96141da792ec368d66ad5f1
João Franklin Goes Amorin <jaofga@gmail.com>
2026-02-11 14:21:11 -0300

    Create entrada e saída - 2.py

---
+++ entrada e saída - 2.py
@@ -0,0 +1,6 @@
+# 2. Faça um programa que: - receba três números inteiros, - calcule e mostre a soma desses números.
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+num3 = int(input('Digite o terceiro número: '))
+resultado = num1 + num2 + num3
+print('O resultado da soma dos 3 número digitados é: ', resultado)
