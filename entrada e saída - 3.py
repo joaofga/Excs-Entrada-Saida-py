9da5227acdd3406df9383629e972df8e1c0d4de5
João Franklin Goes Amorin <jaofga@gmail.com>
2026-02-11 14:24:47 -0300

    Create entrada e saída - 3.py

---
+++ entrada e saída - 3.py
@@ -0,0 +1,6 @@
+# 3. Faça um programa que receba três notas, calcule e mostre a média aritmética.
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+num3 = int(input('Digite o terceiro número: '))
+resultado = (num1 + num2 + num3) / 3
+print('A média dos 3 número digitados é de: ', resultado)
