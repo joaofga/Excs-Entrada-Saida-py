c74dfed89c69a212614925c91e81851c41b87e70
João Franklin Goes Amorin <jaofga@gmail.com>
2026-02-11 14:12:41 -0300

    Create entradaesaida.py

---
+++ entradaesaida.py
@@ -0,0 +1,4 @@
+# 1. Faça um programa que leia um número, multiplique ele por 2 e apresente o resultado.
+numero = int(input('Digite um número: '))
+resultado = numero * 2
+print('O resultado do número digitado multiplicado por 2 é: ', resultado)
