9a20ae77bb564e5aaf26f5f3c0d3b827fcc26603
João Franklin Goes Amorin <jaofga@gmail.com>
2026-02-11 14:29:52 -0300

    Create entrada e saída - 4.py

---
+++ entrada e saída - 4.py
@@ -0,0 +1,7 @@
+# 4. Faça um programa que: receba o salário de um funcionário / calcule e mostre o novo salário, sabendo-se que este sofreu um aumento de 25%.
+# LEIA salario
+# novoSalario = salario + (salario * 25 / 100)
+# ESCREVA novoSalario
+salario = float(input('Digite o salário aqui: '))
+novosalario = salario + (salario * 25 / 100)
+print('O resultado do salário digitado com um aumento de 25% é de: ', novosalario)
