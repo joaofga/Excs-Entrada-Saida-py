acc55e6aa9c9a136a562124c4d6538980b3a0bdd
João Franklin Goes Amorin <jaofga@gmail.com>
2026-02-26 16:28:31 -0300

    entrada e saída - 12

---
+++ entrada e saída - 12.py
@@ -0,0 +1,6 @@
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+num1elev = num1 ** num2
+num2elev = num2 ** num1
+print('O resultado do 1º número elevado pelo 2º é: ', num1elev)
+print('O resultado do 2º número elevado pelo 1º é: ', num2elev)
